7f0a2b637e130c62ca8521bca711246f9c79ff24
jack1776 <john@lexinfra.com>
2013-07-13 22:39:13 +0000

    declare .js within web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var text = fs.readFileSync('index.html','utf8')
-  response.send(text);
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
