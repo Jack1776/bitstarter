f744dd549a2e2a95195799fcc403ba7241b169be
jack1776 <john@lexinfra.com>
2013-07-13 22:47:57 +0000

    respond with the text variable from within web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var text = fs.readFileSync('index.html','utf8')
-  response.send('Hello World2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
